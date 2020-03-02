b98b7db8161784146f62ca6a3462922954032da3
Luis david <david12.29@hotmail.com>
2020-03-01 20:09:56 -0500

    git

--- components/contact.js
+++ components/contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import linkendin from "../images/linkedin.jpg";
 import instagram from "../images/instagram.PNG";
+import githup from "../images/githup.PNG";
 class contact extends React.Component{
     render(){
         return( 
@@ -8,7 +9,7 @@ class contact extends React.Component{
                 <h4>If you want to use my services, you can contact me through the following way.</h4>
                 <img src={linkendin}></img>
                 <img src={instagram}></img>
-                <img></img>
+                <img src={githup}></img>
             </div>
                 
         )
