88eefda9978525ab523fe03fddd7306317f7aacc
Luis david <david12.29@hotmail.com>
2020-03-01 20:08:47 -0500

    changed in contact

--- components/contact.js
+++ components/contact.js
@@ -1,10 +1,14 @@
 import React from "react";
-
+import linkendin from "../images/linkedin.jpg";
+import instagram from "../images/instagram.PNG";
 class contact extends React.Component{
     render(){
         return( 
             <div>
                 <h4>If you want to use my services, you can contact me through the following way.</h4>
+                <img src={linkendin}></img>
+                <img src={instagram}></img>
+                <img></img>
             </div>
                 
         )
