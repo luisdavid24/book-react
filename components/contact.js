0d6d3fa9a889b60eefaa2fa06ef34f43cce5ec03
Luis david <david12.29@hotmail.com>
2020-03-01 20:23:00 -0500

    work in contact

--- components/contact.js
+++ components/contact.js
@@ -2,14 +2,17 @@ import React from "react";
 import linkendin from "../images/linkedin.jpg";
 import instagram from "../images/instagram.PNG";
 import githup from "../images/githup.PNG";
+import "./styles/contact.css";
 class contact extends React.Component{
     render(){
         return( 
-            <div>
+            <div className="container-contact">
                 <h4>If you want to use my services, you can contact me through the following way.</h4>
-                <img src={linkendin}></img>
-                <img src={instagram}></img>
-                <img src={githup}></img>
+                <div className="img-container">
+                    <img  className="contact-img" src={linkendin}></img>
+                    <img  className="contact-img"src={instagram}></img>
+                    <img className="contact-img" src={githup}></img>
+                </div>
             </div>
                 
         )
