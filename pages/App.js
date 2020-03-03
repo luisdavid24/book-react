ac5532625aac4dd81ea358df007e797f55f2df53
Luis david <david12.29@hotmail.com>
2020-03-02 22:44:51 -0500

    changed

--- pages/App.js
+++ pages/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import {BrowserRouter, Switch,Route} from "react-router-dom";
-import Navar from "../components/navar.js";
-import Home from "../components/ice.js";
-import Cream from "../components/cream.js";
-import Milk from "../components/milk.js";
-import Water from "../components/water.js";
-import NotFound from "../components/notFound";
-import Layout from "../components/layout";
+import Book from "../components/book";
+// import NotFound from "../components/notFound";
+
 function App(){
     return (
         <BrowserRouter>
             <Layout>
                 <Switch>
-                    <Route exact path="/Home" component={Home}/>
+                    <Route exact path="/Book" component={Book}/>
                     <Route exact path="/Home/Cream" component={Cream}/>
                     <Route exact path="/Home/Milk" component={Milk}/>
                     <Route exact path="/Home/Water" component={Water}/>
