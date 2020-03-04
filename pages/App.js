8e47129a565374ac1ee0640876df992be8b96fbf
Luis david <david12.29@hotmail.com>
2020-03-03 22:23:34 -0500

    layout

--- pages/App.js
+++ pages/App.js
@@ -14,6 +14,7 @@ import Throne from "../components/throne";
 import Testimony from "../components/testimony";
 import Footer from "../components/footer";
 import Contact from "../components/contact";
+import Layout from "../components/Layout";
 // import NotFound from "../components/notFound";
 
 function App(){
