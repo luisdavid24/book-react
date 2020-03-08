1f4be38a1632f67cc7ab0479f2cdf3876f9a3afd
Luis david <david12.29@hotmail.com>
2020-03-07 21:21:11 -0500

    i am working in the home

--- components/home.js
+++ components/home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./styles/home.css";
 class home extends React.Component{
     render(){
         return( 
             <div>
                 <h1>MY BOOKS</h1>
+                <p>You will see all the books I have read or tried to read. I hope you like my website.</p>
             </div>
                 
         )
