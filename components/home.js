75f81a4956dd133dc10446c5c55e51477f4cef3c
Luis david <david12.29@hotmail.com>
2020-03-07 21:29:19 -0500

    i changed the home

--- components/home.js
+++ components/home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./styles/home.css";
+import Libro from "../images/libro.jpg";
 class home extends React.Component{
     render(){
         return( 
-            <div>
+            <div className="home-container">
                 <h1>MY BOOKS</h1>
                 <p>You will see all the books I have read or tried to read. I hope you like my website.</p>
+                <img  className="home-phontos"   src={Libro}/>
             </div>
                 
         )
