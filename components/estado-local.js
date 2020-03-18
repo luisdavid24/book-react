91ba75d16d892e0153ecc9c90d7d61dbd8ef286d
Luis david <david12.29@hotmail.com>
2020-03-17 22:05:42 -0500

    constructor

--- components/estado-local.js
+++ components/estado-local.js
@@ -1,34 +1,36 @@
-// class Clock extends React.Component {
-//     render() {
-//       return (
-//         <div>
-//           <h1>Hello, world!</h1>
-//           <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-//         </div>
-//       );
-//     }
-//   }
-// 
+//Reemplazar this.props.datecon this.state.dateen el render()método
+class Clock extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
 
-//Aqui se agrega un constructor que asigne la inicial this.state
-// class Clock extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {date: new Date()};
-//     }
+
+// Aqui se agrega un constructor que asigne la inicial this.state
+//super es usada para acceder y llamar funciones del padre de un objeto.
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
   
-//     render() {
-//       return (
-//         <div>
-//           <h1>Hello, world!</h1>
-//           <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-//         </div>
-//       );
-//     }
-//   }
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
 
-// Tenga en cuenta cómo pasamos propsal constructor base
-// constructor(props) {
-//     super(props);
-//     this.state = {date: new Date()};
-//   }
+// Tenga en cuenta cómo pasamos props al constructor base
+constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+}
