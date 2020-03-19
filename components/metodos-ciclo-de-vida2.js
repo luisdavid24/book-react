26326efcd6555929973532761fd0600e1a1f8322
Luis david <david12.29@hotmail.com>
2020-03-18 21:53:59 -0500

    life

---
+++ components/metodos-ciclo-de-vida2.js
@@ -0,0 +1,37 @@
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    }); //implementaremos un método llamado tick()que el Clockcomponente se ejecutará cada segundo.
+  }//Se utilizará this.setState()para programar actualizaciones para el estado local del componente
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
