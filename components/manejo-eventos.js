18625a8cfb8a690be11a18c55daf8f85df707d39
Luis david <david12.29@hotmail.com>
2020-03-20 15:40:00 -0500

    event

---
+++ components/manejo-eventos.js
@@ -0,0 +1,4 @@
+// HTML simple, para evitar el comportamiento de enlace predeterminado al abrir una nueva página, puede escribir
+<a href="#" onclick="console.log('The link was clicked.'); return false">
+  Click me
+</a>
