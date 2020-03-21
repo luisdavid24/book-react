ef960785aa6f918d387c3374ade7ec46d9f381c4
Luis david <david12.29@hotmail.com>
2020-03-21 16:25:44 -0500

    changed

---
+++ components/operador-condicional.js
@@ -0,0 +1,9 @@
+render() {
+  const isLoggedIn = this.state.isLoggedIn;
+  return (
+    <div>
+      The user is <b>{isLoggedIn ? 'currently' : 'not'}</b> logged in.
+    </div>
+  );
+}
+//En este codigo se analiza si el elemento es currently o not usando esta sintaxis {isLoggedIn ? 'currently' : 'not'}.
