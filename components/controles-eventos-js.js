93201ccc24192000bf323cc88dc6a5ac33a5a30a
Luis david <david12.29@hotmail.com>
2020-03-21 15:12:37 -0500

    changed

--- components/controles-eventos-js.js
+++ components/controles-eventos-js.js
@@ -1,10 +1,10 @@
 function UserGreeting(props) {
   return <h1>Welcome back!</h1>;
-}
+}//Este elemento se muestra cuando ya te logiaste como ejemplo.
 
 function GuestGreeting(props) {
   return <h1>Please sign up.</h1>;
-}
+}//Este elemento se muestra cuando todavia no te logiaste como ejemplo.
 
 function Greeting(props) {
   const isLoggedIn = props.isLoggedIn;
@@ -18,4 +18,10 @@ ReactDOM.render(
   //Try changing to isLoggedIn={true}:
   <Greeting isLoggedIn={false} />,
   document.getElementById('root')
+  //el isLoggein puede ser true o false segun el caso y muestra 
 );
+
+
+
+
+
