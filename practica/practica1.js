9be2e8ab696f7f848c7c08c5255e5430323f845e
Luis david <david12.29@hotmail.com>
2020-03-22 16:28:54 -0500

    i working in a button

--- practica/practica1.js
+++ practica/practica1.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./styles/practica1.css";
 class Practica1 extends React.Component{
+    constructor(props){
+    super(props);
+    this.buttonLogin=this.buttonLogin.bind(this);
+    this.buttonLogout=this.buttonLogout.bind(this);
+    this.state={isLogin:false};
+    }
+    buttonLogin(){
+        this.setState({isLogin:true})
+    }
+    buttonLogout(){
+        this.setState({isLogin:false})
+    }
+
     render(){
         return(
             <div className="container">
                 <h1>Hello, this is my first practice</h1>
-                <Boton/>
+                {text}
+                <Boton />
             </div>
 
         );
@@ -15,7 +29,22 @@ class Practica1 extends React.Component{
 function Boton(){
     return(
         <div>
-            <button> Touch me,please</button>
+            <button onClick={this.props.onClick}> Touch me,please</button>
+        </div>
+    )
+}
+function textAntes(){
+    return(
+        <div>
+            <p>Por favor toca el boton para empezar</p>
+        </div>
+    )
+}
+
+function textDespues(){
+    return(
+        <div>
+            <p>Excelente,si tocaste</p>
         </div>
     )
 }
